c08a5f66597a38f887289c066b4d1e6e878cc375
Ol-Lav <arnab@ukr.net>
2022-07-28 18:09:15 +0300

    Initial

--- src/Components/Image/Image.js
+++ src/Components/Image/Image.js
@@ -1,10 +1,9 @@
 import "./Image.css";
 
 export const Image = ({ src, alt }) => {
-
   return (
-    <div className="image">
-      <img src={src} alt={alt} />
+    <div className="image__container">
+      <img src={src} alt={alt} className="image" />
     </div>
   );
 };
